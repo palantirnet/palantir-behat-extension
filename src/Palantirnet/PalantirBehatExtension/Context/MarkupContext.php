1c2d2948b4abf76bbf494c5fadd31aafc5a858f4
Bec White <white@palantir.net>
2015-10-20 13:57:52 -0500

    Add a custom markup context.

---
+++ src/Palantirnet/PalantirBehatExtension/Context/MarkupContext.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @file
+ * Behat context class with functionality that is shared across custom contexts.
+ *
+ * @copyright (c) Copyright 2015 Palantir.net, Inc.
+ */
+
+namespace Palantirnet\PalantirBehatExtension\Context;
+
+use Drupal\DrupalExtension\Context\MarkupContext as DrupalExtensionMarkupContext;
+
+class MarkupContext extends DrupalExtensionMarkupContext {
+
+    /**
+     * @Then I dump the :region region
+     */
+    public function dumpRegion($region)
+    {
+        print $this->getRegion($region)->getHTML() . "\r\n";
+    }
+
+    /**
+     * @Then I dump the URL
+     */
+    public function dumpUrl()
+    {
+        print $this->getSession()->getCurrentUrl() . "\r\n";
+    }
+
+    /**
+     * @Then I( should) see :text in the :tag element in the :region( region)
+     */
+    public function assertRegionElementText($text, $tag, $region) {
+        $regionObj = $this->getRegion($region);
+        $results = $regionObj->findAll('css', $tag);
+
+        $found = FALSE;
+        if (!empty($results)) {
+            foreach ($results as $result) {
+                if ($result->getText() == $text) {
+                    $found = TRUE;
+                }
+            }
+        }
+
+        if (!$found) {
+            throw new \Exception(sprintf('The text "%s" was not found in the "%s" element in the "%s" region on the page %s', $text, $tag, $region, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * @Then I should not see :text in the :tag element in the :region( region)
+     */
+    public function assertNotRegionElementText($text, $tag, $region) {
+        $regionObj = $this->getRegion($region);
+        $results = $regionObj->findAll('css', $tag);
+
+        $found = FALSE;
+        if (!empty($results)) {
+            foreach ($results as $result) {
+                if ($result->getText() == $text) {
+                    $found = TRUE;
+                }
+            }
+        }
+
+        if ($found) {
+            throw new \Exception(sprintf('The text "%s" was found in the "%s" element in the "%s" region on the page %s', $text, $tag, $region, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * @Then I should see a/an :label field
+     */
+    public function assertFieldByLabel($label)
+    {
+        $page = $this->getSession()->getPage();
+        $field = $page->findField($label);
+
+        if (null === $field) {
+            throw new \Exception(sprintf('Field with label "%s" was not found on the page %s.', $label, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * @Then I should not see a/an :label field
+     */
+    public function assertNotFieldByLabel($label)
+    {
+        try {
+            $this->assertFieldByLabel($label);
+        }
+        catch (\Exception $e) {
+            return;
+        }
+
+        throw new \Exception(sprintf('Found field with label "%s" on page %s', $label, $this->getSession()->getCurrentUrl()));
+    }
+
+    /**
+     * @Then I should see a/an :label multivalue field
+     */
+    public function assertFieldsetByLabel($label)
+    {
+        $page = $this->getSession()->getPage();
+        $legend = $this->getSession()->getSelectorsHandler()->xpathLiteral($label);
+        $fieldset = $page->find('named', array('fieldset', $legend));
+
+        if (null === $fieldset) {
+            throw new \Exception(sprintf('The multivalue field with label "%s" was not found on the page %s.', $label, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+}
