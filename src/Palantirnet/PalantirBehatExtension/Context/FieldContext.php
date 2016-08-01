9da68826e70b32922e5d581b8648d9c2f176627e
Andrea R Soper <soper@palantir.net>
2016-08-01 13:44:40 -0500

    Add a Field context to check for the existence of fields.

---
+++ src/Palantirnet/PalantirBehatExtension/Context/FieldContext.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Contains assertions to test for field types on a form.
+ *
+ * @copyright Copyright (c) 2016 Palantir.net
+ */
+namespace Palantirnet\PalantirBehatExtension\Context;
+
