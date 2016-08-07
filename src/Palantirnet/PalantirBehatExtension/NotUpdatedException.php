b5f52b6b8533d280a22b7eb3d19667109d2f8cbd
Andrea R Soper <soper@palantir.net>
2016-08-07 17:11:06 -0500

    Remove overiding construct. It was broken.

--- src/Palantirnet/PalantirBehatExtension/NotUpdatedException.php
+++ src/Palantirnet/PalantirBehatExtension/NotUpdatedException.php
@@ -14,21 +14,4 @@ use Symfony\Component\Config\Definition\Exception\InvalidDefinitionException;
  */
 class NotUpdatedException extends InvalidDefinitionException implements ContextException
 {
-
-
-    /**
-     * Construct the NotUpdatedException.
-     *
-     * @param String     $message  [optional] The Exception message to throw.
-     * @param Int        $code     [optional] The Exception code.
-     * @param \Exception $previous [optional] The previous exception used.
-     */
-    public function __construct($message, $code, \Exception $previous)
-    {
-        $this->message = "Context not updated for Drupal 8. $message";
-        parent::__construct($message, $code, $previous);
-
-    }//end __construct()
-
-
 }//end class
